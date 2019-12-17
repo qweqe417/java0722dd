31c264064ef2c2a391f4b8f1b197afad5baad726
java0722 <test@qq.com>
2019-12-17 19:24:52 +0800

    test commit

---
+++ 1.java
@@ -0,0 +1,14 @@
+public class Mycccc {
+    public static void main(String[] args) {
+        int  size  =1024*1224;
+        byte[]  sy = new byte[3*size];
+        System.out.println(11111);
+        byte[]  sy1 = new byte[3*size];
+        System.out.println(22222);
+        byte[]  sy2 = new byte[3*size];
+        System.out.println(33333);
+        byte[]  sy3 = new byte[3*size];
+        System.out.println(4444);
+    }
+
+}
