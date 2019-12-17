b510705237b0dfeeada08da82e4f6f27b4dc0b58
qweqe417 <56719201+qweqe417@users.noreply.github.com>
2019-12-17 19:30:16 +0800

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,5 +1,7 @@
 public class Mycccc {
     public static void main(String[] args) {
+      
+        1111111
         int  size  =1024*1224;
         byte[]  sy = new byte[3*size];
         System.out.println(11111);
